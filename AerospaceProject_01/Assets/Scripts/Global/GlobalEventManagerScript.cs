75d276ac45909b9fa821b28a8b76bae87feb6a93
ChenXiaoqiang <15330223674@163.com>
2019-03-07 19:50:13 +0800

    GlobalEventManagerScript.cs脚本。该脚本包含着对一些脚本中方法的监听和取消监听。

---
+++ AerospaceProject_01/Assets/Scripts/Global/GlobalEventManagerScript.cs
@@ -0,0 +1,29 @@
+﻿using Optoma.FileManager;
+using Optoma.Command;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Optoma.Global
+{
+    /// <summary>
+    ///  全局时间管理
+    /// </summary>
+    public class GlobalEventManagerScript : MonoBehaviour
+    {
+        private void Awake()
+        {
+            // 监听事件
+            EventCenter.AddListener<string>(GlobalConfig.EnumTypesManager.EventTypes.OpenDirecory,
+                OpenDirectoryChooseFile.OpenDirectoryAndChooseFile);
+        }
+
+        private void OnDestroy()
+        {
+            // 取消监听
+            EventCenter.RemoveListener<string>(GlobalConfig.EnumTypesManager.EventTypes.OpenDirecory,
+                OpenDirectoryChooseFile.OpenDirectoryAndChooseFile);
+            Application.Quit();
+        }
+    }
+}
