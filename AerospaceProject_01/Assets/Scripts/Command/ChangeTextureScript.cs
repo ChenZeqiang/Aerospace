30b78f3c2a7420b400a64abac9254854dccb3c88
ChenXiaoqiang <15330223674@163.com>
2019-03-07 19:33:45 +0800

    ChangeTextureScript.cs脚本,该脚本作用在可以显示图片的物体上。包含可以修改显示的图片的方法。

---
+++ AerospaceProject_01/Assets/Scripts/Command/ChangeTextureScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Optoma.Command;
+using Optoma.Global;
+
+namespace Optoma.FileManager
+{
+    /// <summary>
+    ///  改变物体自身的图片
+    /// </summary>
+    public class ChangeTextureScript : MonoBehaviour
+    {
+        #region 属性变量
+
+
+
+        #endregion
+
+        #region Unity Callback
+        private void Awake()
+        {
+           
+        }
+        #endregion
+    }
+}
