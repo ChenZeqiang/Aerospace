89012bc2d2a6314a037729fb55d9867a673f2e71
ChenXiaoqiang <15330223674@163.com>
2019-03-07 19:39:33 +0800

    OpenExplorerScript.cs脚本。该脚本添加到Button按钮上，当点击按钮的时候，会广播打开磁盘的事件。

---
+++ AerospaceProject_01/Assets/Scripts/Command/OpenExplorerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Optoma.FileManager;
+using Optoma.Global;
+using UnityEngine.UI;
+
+namespace Optoma.Command
+{
+    /// <summary>
+    ///  打开文件管理器
+    /// </summary>
+    public class OpenExplorerScript : MonoBehaviour
+    {
+        #region Unity Callback
+
+        private void Start()
+        {
+            gameObject.GetComponent<Button>().onClick.AddListener(delegate() {
+                // 进行广播，打开文件管理器
+                EventCenter.Broadcast<string>(GlobalConfig.EnumTypesManager.EventTypes.OpenDirecory,
+                    GlobalConfig.FileTypesManager.FileType);
+            });
+        }
+
+        #endregion
+    }
+}
